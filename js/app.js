8fee828445cb911db5d4c8393cc11bd06aea6c34
AgusBulacio <agusbulacio1803@gmail.com>
2025-07-15 18:12:36 -0300

    corregi la funcion para mostrar resultados

--- js/app.js
+++ js/app.js
@@ -1,36 +1,29 @@
 let numeroAleatorio = 0;
+let numerosIngresados = [];
 
 
 function comenzarJuego(){
-    console.log('Enviado desde la funcion comenzarJugo');
     numeroAleatorio = Math.floor(Math.random() * 10);
-    console.log(numeroAleatorio);
 }
 
 function recibirNumero(e){
     e.preventDefault();
-    console.log(e);
-    console.log('Enviado desde la funcion recibirNumero');
     const input = document.querySelector('.form-control');
      numeroRecibido = parseInt (input.value);
+     numerosIngresados.push(numeroRecibido);
      mostrarResultado(numeroAleatorio, numeroRecibido);
-     formulario.reset();
-
-     
+     formulario.reset();     
 }
 
 function mostrarResultado(numeroAleatorio, numeroRecibido){
     if(numeroAleatorio === numeroRecibido){
         alert(`Adivinaste el numero! ${numeroRecibido}`);
     }else if(numeroAleatorio > numeroRecibido){
-        alert(`El numero ingresado ${numeroRecibido} es menor que el numero generado ${numeroAleatorio}`);
+        alert(`El numero ingresado ${numeroRecibido} es menor que el numero generado`);
     }else{
-        alert(`El numero ingresado ${numeroRecibido} es mayor que el numero generado ${numeroAleatorio}`);
+        alert(`El numero ingresado ${numeroRecibido} es mayor que el numero generado`);
     }
 }
-
-
-
 
 const btnComenzar = document.querySelector('.btn-primary');
 const formulario = document.querySelector('#formNumero');
